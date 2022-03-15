bf23eae9d46f519325fe711ead23647aafd57c5a
Abdisa Alemu <abdisa.alemu.wako@gmail.com>
2022-03-14 22:17:04 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	 printf('_');
+	 printf('p');
+	 printf('u');
+	 printf('t');
+	 printf('c');
+	 printf('h');
+	 printf('a');
+	 printf('r');
+	 printf('\n');
+
+	return (0);
+}
