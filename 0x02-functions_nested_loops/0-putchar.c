b076597b94c2612355f7203f469fa82668fcb341
Abdisa Alemu <abdisa.alemu.wako@gmail.com>
2022-03-15 12:00:54 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,17 @@
 #include "main.h"
 /**
- *main - Entry point
- *Return: Always zero
- */
-
+ * main - main block
+ * 
+ * Return: 0
+ **/
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('n');
-	_putchar('\n');
+	char x[] = "_putchar";
 
+	for (int k  = 0; k < 8; k++)
+	{
+		_putchar(x[k]);
+	}
+	_putchar('\n');
 	return (0);
 }}
