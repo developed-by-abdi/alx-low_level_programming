343292a3c963b949819987ab55243d2c6dd4cdfb
Abdisa Alemu <abdisa.alemu.wako@gmail.com>
2022-03-15 12:37:16 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ **/
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+		_putchar(k);
+	_putchar('\n');
+}
