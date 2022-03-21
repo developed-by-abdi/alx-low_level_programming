75f8a54334b7d30bc00e462a50cbdf34be2fa14c
Abdisa Alemu <abdisa.alemu.wako@gmail.com>
2022-03-21 13:20:03 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+int _strlen(const char *str)
+{
+	int length = 0;
+
+	while (*str++)
+		length++;
+
+	return (length);
+}
