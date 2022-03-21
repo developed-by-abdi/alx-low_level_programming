643135915ee623edc9c5278439379f52e47b9d07
Abdisa Alemu <abdisa.alemu.wako@gmail.com>
2022-03-21 13:18:36 -0700

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#include <studio.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -8,3 +10,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
+#endif
