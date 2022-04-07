b575fbc1361a95d7f3d444efa29ba39130d4a35d
Abdisa Alemu <abdisa.alemu.wako@gmail.com>
2022-04-07 11:28:42 -0700

    main

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,4 +8,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif;
+#endif
