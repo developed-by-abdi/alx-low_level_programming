9c90bdbeac04d809e070e7c5cfc97fd5c4f5939d
Abdisa Alemu <abdisa.alemu.wako@gmail.com>
2022-04-21 12:24:11 -0700

    main

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,17 +1,23 @@
-#ifndef LISTS_H
+[200~#ifndef LISTS_H
 #define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+		char *str;
+			unsigned int len;
+				struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+int _strlen(const char *);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void print(void) __attribute__((constructor));
 
 #endif
